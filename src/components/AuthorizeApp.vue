103a17e8825890afe894396a34e31af5e1814898
elf Pavlik <elf-pavlik@hackers4peace.net>
2023-08-31 12:47:19 -0600

    optional access needs when authorizing apps
    Co-authored-by: Maciej Samoraj <maciej.samoraj@gmail.com>

--- src/components/AuthorizeApp.vue
+++ src/components/AuthorizeApp.vue
@@ -4,21 +4,54 @@
     :prepend-avatar="application.logo"
   >
     <v-card-text>
-      <v-list>
+      <v-list
+        v-for="need of authorizationData.accessNeedGroup.needs"
+      >
         <v-list-item
-          v-for="need of authorizationData.accessNeedGroup.needs"
-          :title="need.shapeTree.label"
-          :subtitle="need.label"
           lines="three"
+          @click="toggleSelect(need.id)"
         >
-          <v-list v-if="need.children">
-            <v-list-item
-              v-for="child in need.children"
-              :title="child.shapeTree.label"
-              :subtitle="child.label"
-            > </v-list-item>
-          </v-list>
+          <v-list-item-title>
+            <v-icon :icon="chooseIcon(need.access)"></v-icon>
+            {{ need.shapeTree.label}}
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            {{ need.label }}
+          </v-list-item-subtitle>
+          <template v-slot:append>
+            <v-list-item-action end>
+              <v-checkbox-btn
+                :model-value="isSelected(need.id)"
+                :disabled="need.required"
+                @click.prevent
+              ></v-checkbox-btn>
+            </v-list-item-action>
+          </template>
         </v-list-item>
+        <v-list-item
+          class="ml-3"
+          v-if="need.children"
+          v-for="child in need.children"
+          lines="three"
+          @click="toggleSelect(need.id, child.id)"
+        >
+          <v-list-item-title>
+            <v-icon :icon="chooseIcon(child.access)"></v-icon>
+            {{ child.shapeTree.label}}
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            {{ child.label }}
+          </v-list-item-subtitle>
+        <template v-slot:append>
+          <v-list-item-action end>
+            <v-checkbox-btn
+              :model-value="isSelected(need.id, child.id)"
+              :disabled="child.required"
+              @click.prevent
+            ></v-checkbox-btn>
+          </v-list-item-action>
+        </template>
+      </v-list-item>
       </v-list>
     </v-card-text>
     <div class="px-2 d-flex justify-space-between">
@@ -42,8 +75,8 @@
 </template>
 
 <script lang="ts" setup>
-import { AccessNeed, Application, Authorization, AuthorizationData, BaseAuthorization, DataAuthorization } from '@janeirodigital/sai-api-messages';
-import { ref, watch } from 'vue';
+import { AccessModes, AccessNeed, Application, Authorization, AuthorizationData, BaseAuthorization, DataAuthorization } from '@janeirodigital/sai-api-messages';
+import { reactive, ref, watch } from 'vue';
 import { useAppStore } from '@/store/app'
 
 const appStore = useAppStore()
@@ -55,6 +88,48 @@ const props = defineProps<{
 
 const loadingAuthorize = ref(false)
 const loadingDeny = ref(false)
+const selection = reactive({
+  needs: props.authorizationData.accessNeedGroup.needs.map(n => ({
+    id: n.id,
+    required: n.required,
+    selected: true,
+    children: n.children?.map(c => ({
+      id: c.id,
+      required: c.required,
+      selected: true
+    }))
+  }))
+})
+
+function toggleSelect(needId: string, childId?: string): void {
+  const need = selection.needs.find(n => n.id == needId)!
+  if (childId) {
+    const child = need.children?.find(n => n.id == childId)!
+    if (child.required) return
+    child.selected = !child.selected
+  } else {
+    if (need.required) return
+    need.selected = !need.selected
+  }
+}
+
+function isSelected(needId: string, childId?: string): boolean {
+  const need = selection.needs.find(n => n.id == needId)!
+  if (childId) {
+    const child = need.children?.find(n => n.id == childId)!
+    return child.selected
+  } else {
+    return need.selected
+  }
+}
+
+function chooseIcon(access: string[]): string {
+  if(access.includes(AccessModes.Update)) {
+    return 'mdi-square-edit-outline'
+  } else {
+    return 'mdi-euey-outline'
+  }
+}
 
 function getScope(accessNeed: AccessNeed): string {
   if (accessNeed.parent) return 'Inherited'
@@ -64,6 +139,11 @@ function getScope(accessNeed: AccessNeed): string {
 
 // TODO use case when there is a `parent` access need
 function createDataAuthorizations(accessNeed: AccessNeed, parent?: AccessNeed): DataAuthorization[] {
+  if (parent) {
+    if (!isSelected(parent.id, accessNeed.id)) return []
+  } else {
+    if (!isSelected(accessNeed.id)) return []
+  }
   const dataAuthorization = {
     accessNeed: accessNeed.id,
     scope: getScope(accessNeed),
